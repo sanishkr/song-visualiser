d2499f6ad8e4e6d744585c3b94ab7cdd5e6eb73c
sanishkr <sanishkr@gmail.com>
2020-11-28 23:35:02 +0530

    Current Playing Spotify Completed
    * login/auth
    * get details of album, track, artist
    * handle cases like ad playing or no device connected
    * icons for various controls
    * durations

---
+++ pages/App.js
@@ -0,0 +1,171 @@
+import React, { Component } from 'react';
+import dynamic from 'next/dynamic';
+import { withRouter } from 'next/router';
+import { authEndpoint, clientId, redirectUri, scopes } from './config';
+import Player from './Player';
+// import logo from "./logo.svg";
+import './App.css';
+
+// const hash = dynamic(() => import('./hash'))
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+      item: {
+        album: {
+          images: [{ url: '' }],
+        },
+        name: '',
+        artists: [{ name: '' }],
+        duration_ms: 0,
+      },
+      currently_playing_type: '',
+      is_playing: 'Paused',
+      progress_ms: 0,
+      no_data: false,
+      repeat_state: '',
+      shuffle_state: false,
+      device: {},
+    };
+
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    const { router } = this.props;
+    const hash = router.asPath.includes('#')
+      ? this.tokenize(router.asPath.split('#')[1])
+      : {};
+    // Set token
+    let _token = hash.access_token;
+
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token,
+      });
+      this.getCurrentlyPlaying(_token);
+      // this.getLyrics()
+    }
+
+    // set interval for polling every 5 seconds
+    this.interval = setInterval(() => this.tick(), 5000);
+  }
+
+  tokenize(path) {
+    return path.split('&').reduce(function(initial, item) {
+      if (item) {
+        var parts = item.split('=');
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+      }
+      return initial;
+    }, {});
+  }
+
+  componentWillUnmount() {
+    // clear the interval to save resources
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    if (this.state.token) {
+      this.getCurrentlyPlaying(this.state.token);
+    }
+  }
+
+  getCurrentlyPlaying(token) {
+    // Make a call using the token
+    fetch('https://api.spotify.com/v1/me/player', {
+      method: 'get',
+      headers: new Headers({
+        Authorization: 'Bearer ' + token,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        // Checks if the data is not empty
+        // console.table(data);
+        if (!data) {
+          this.setState({
+            no_data: true,
+          });
+          return;
+        }
+
+        this.setState(
+          {
+            item: data.item,
+            is_playing: data.is_playing,
+            progress_ms: data.progress_ms,
+            repeat_state: data.repeat_state,
+            shuffle_state: data.shuffle_state,
+            currently_playing_type: data.currently_playing_type,
+            device: data.device,
+            no_data: false /* We need to "reset" the boolean, in case the
+                          user does not give F5 and has opened his Spotify. */,
+          },
+          () => {
+            // this.getLyrics()
+          },
+        );
+      })
+      .catch(e => {
+        console.error(e);
+        this.setState({
+          no_data: true,
+        });
+      });
+  }
+
+  getLyrics() {
+    const { artists, name } = this.state.item.album;
+    console.log({ name }, artists[0]['name']);
+    fetch(
+      `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?format=json&q_track=${name}t&q_artist=${artists[0]['name']}&apikey=d6f65cfc34495d4df7b232e3c5315f46`,
+    )
+      .then(res => res.json())
+      .then(data => console.log(data.message.body?.lyrics?.lyrics_body ?? ''));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          {!this.state.token && (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                '%20',
+              )}&response_type=token&show_dialog=true`}
+            >
+              Login to Spotify
+            </a>
+          )}
+          {this.state.token && !this.state.no_data && (
+            <Player
+              item={this.state.item}
+              is_playing={this.state.is_playing}
+              progress_ms={this.state.progress_ms}
+              repeat_state={this.state.repeat_state}
+              shuffle_state={this.state.shuffle_state}
+              device={this.state.device}
+              currently_playing_type={this.state.currently_playing_type}
+            />
+          )}
+          {this.state.no_data && (
+            <p>
+              You need to be playing a song on Spotify, for something to appear
+              here.
+            </p>
+          )}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default withRouter(App);
---
+++ pages/Player.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import './Player.css';
+
+const Player = props => {
+  const backgroundStyles = {
+    backgroundImage: `url(${
+      props.currently_playing_type === 'ad'
+        ? '/images/spotify-ad.jpeg'
+        : props.item.album.images[0].url
+    })`,
+  };
+
+  const getDisplayTime = durationInMS => {
+    const min = Math.floor(durationInMS / (1000 * 60));
+    const sec = Math.floor(durationInMS / 1000) % 60;
+    return pad(min) + ':' + pad(sec);
+  };
+
+  function pad(n) {
+    return n < 10 ? '0' + n : n;
+  }
+
+  const progressBarStyles = {
+    width:
+      props.currently_playing_type === 'ad'
+        ? 0
+        : (props.progress_ms * 100) / props.item.duration_ms + '%',
+  };
+
+  const getVolumeIcon = volume => {
+    if (volume === 0) {
+      return '/images/icons8-mute-50.png';
+    } else if (volume <= 33) {
+      return '/images/icons8-low-volume-50.png';
+    } else if (volume <= 66) {
+      return '/images/icons8-voice-50.png';
+    } else {
+      return '/images/icons8-audio-50.png';
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="main-wrapper">
+        <div className="my-4 now-playing__img">
+          <img
+            src={
+              props.currently_playing_type === 'ad'
+                ? '/images/spotify-ad.jpeg'
+                : props.item.album.images[0].url
+            }
+          />
+        </div>
+        <div className="now-playing__side">
+          <div className="now-playing__name">
+            {props.currently_playing_type === 'ad'
+              ? 'Ad Playing'
+              : props.item.name}
+          </div>
+          <div className="now-playing__artist">
+            <span>
+              {props.currently_playing_type === 'ad'
+                ? 'Spotify Ad'
+                : props.item.artists[0].name}
+            </span>
+          </div>
+          <div className="flex items-center justify-between px-2 my-4 text-sm">
+            <span className="font-semibold now-playing__status">
+              {props.is_playing ? 'Playing' : 'Paused'} on {props.device.name}
+            </span>
+            <img
+              className="h-6"
+              src={getVolumeIcon(props.device.volume_percent)}
+            />
+          </div>
+          <div className="progress">
+            <div className="progress__bar" style={progressBarStyles} />
+          </div>
+          {props.currently_playing_type !== 'ad' ? (
+            <div className="flex items-center justify-between my-4 text-sm">
+              <span>{getDisplayTime(props.progress_ms)}</span>
+              <span>{getDisplayTime(props.item.duration_ms)}</span>
+            </div>
+          ) : null}
+          <div className="flex items-center justify-between mt-4">
+            <img
+              className="h-8"
+              src={
+                props.shuffle_state
+                  ? '/images/icons8-shuffle-on-50.png'
+                  : '/images/icons8-shuffle-off-50.png'
+              }
+            />
+            <img
+              className="h-8"
+              src={
+                props.is_playing
+                  ? '/images/icons8-play-50.png'
+                  : '/images/icons8-pause-50.png'
+              }
+            />
+            <img
+              className="h-8"
+              src={`/images/icons8-repeat-${props.repeat_state}-50.png`}
+            />
+          </div>
+        </div>
+        <div className="background" style={backgroundStyles} />{' '}
+      </div>
+    </div>
+  );
+};
+
+export default Player;
